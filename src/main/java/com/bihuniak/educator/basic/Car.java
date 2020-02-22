b0ffc4f4354fef8b71a7ff4f9ce86e943b981570
Ruqeert <jakub.rupnicki@gmail.com>
2020-02-22 13:34:58 +0100

    Siemano, nowy teacher kazał zrobić new brancha

---
+++ src/main/java/com/bihuniak/educator/basic/Car.java
@@ -0,0 +1,38 @@
+package com.bihuniak.educator.basic;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    public Car(int id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private long id;
+
+    private String model;
+
+
+
+    private String brand;
+    private double engineCapacity;
+    private int wheels;
+    private boolean secondHand;
+
+    public Car(String model, String brand, double engineCapacity, int wheels, boolean secondHand) {
+        this.model = model;
+        this.brand = brand;
+        this.engineCapacity = engineCapacity;
+        this.wheels = wheels;
+        this.secondHand = secondHand;
+    }
+    public Car() {
+
+    }
+}
